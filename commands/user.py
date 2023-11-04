3c98711c7780bf39ccdc503692d948683b6a750f
v1ss0nd <v1ss0nd@yahoo.com>
2023-11-04 17:03:17 +0600

    #user now can see banned users

---
+++ commands/user.py
@@ -0,0 +1,63 @@
+from twitchio.ext import commands
+import requests
+from datetime import datetime
+
+class User(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.cooldown(1, 2, commands.Bucket.user)
+    async def user(self, ctx: commands.Context, name :str = None):
+        name = name.replace ("@", "")
+        name = name.replace (",", "")
+        if name is None:
+            user_name = ctx.author.name
+        else:
+            user_name = name
+
+        url = f'https://api.ivr.fi/v2/twitch/user?login={user_name}'
+        response = requests.get(url)
+        data = response.json()
+        format = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+        try:
+            banned = data[0]['banned']
+            if banned:
+                chatterCount = data[0]['chatterCount']
+                followers = data[0]['followers'] 
+                color = data[0]['chatColor'] 
+                bio = data[0]['bio']
+                prefix = data[0]['emotePrefix']
+                id = data[0]['id']
+                display_name = data[0]['displayName']
+                createdDate = data[0]['createdAt']
+                banReason = data[0]['banReason']
+
+                date_object = datetime.strptime(createdDate, format)
+
+                output_format = "%Y-%m-%d"
+                createdAt = date_object.strftime(output_format)
+                
+                await ctx.reply(f"{id} | @{display_name}, Created at: {createdAt} [BANNED: {banReason}😢] | Color: {color} | Prefix: {prefix} | Followers count: {followers} | Chatters count: {chatterCount} | Bio: {bio}")            
+            else:
+                chatterCount = data[0]['chatterCount']
+                followers = data[0]['followers'] 
+                color = data[0]['chatColor'] 
+                bio = data[0]['bio']
+                prefix = data[0]['emotePrefix']
+                id = data[0]['id']
+                display_name = data[0]['displayName']
+                createdDate = data[0]['createdAt']
+                
+                date_object = datetime.strptime(createdDate, format)
+
+                output_format = "%Y-%m-%d"
+                createdAt = date_object.strftime(output_format)
+
+                await ctx.reply(f"{id} | @{display_name}, Created at: {createdAt} | Color: {color} | Prefix: {prefix} | Followers count: {followers} | Chatters count: {chatterCount} | Bio: {bio}")
+        except Exception:
+            await ctx.reply(f"Cannot find {user_name} FeelsBadMan")
+
+def prepare(bot):
+    bot.add_cog(User(bot))
