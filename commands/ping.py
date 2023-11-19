3a1a086f45b974135af6cfd78ca5d90ef12f195c
v1ss0nd <v1ss0nd@yahoo.com>
2023-11-19 17:03:45 +0600

    Update ping.py

--- commands/ping.py
+++ commands/ping.py
@@ -1,5 +1,6 @@
 from twitchio.ext import commands
 import time
+import psutil
 start_time = time.time()
 
 class Ping(commands.Cog):
@@ -10,10 +11,11 @@ class Ping(commands.Cog):
     @commands.cooldown(1, 5, commands.Bucket.user)
     async def ping(self, ctx: commands.Context):
         try:
+            ram = psutil.virtual_memory()
             uptime = time.time() - start_time
             hours, remainder = divmod(uptime, 3600)
             minutes, seconds = divmod(remainder, 60)
-            await ctx.reply(f'/me 🥱 {int(hours)} hours {int(minutes)} minutes {int(seconds)} seconds')
+            await ctx.reply(f'/me 🥱 {int(hours)} hours {int(minutes)} minutes {int(seconds)} seconds | CPU: {(psutil.cpu_percent())}% | RAM: Used: {ram()[2]} MB, Available: {ram.available / 1024 / 1024 / 1024:.2f} GB, Total: {ram.total / 1024 / 1024 / 1024:.2f} GB')
         except Exception as e:
             await ctx.reply(f'pong! [{e}] ')
 
