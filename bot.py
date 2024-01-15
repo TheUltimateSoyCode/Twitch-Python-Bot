848911c790e9e5bafd58ba9233101c4f27c2fd77
v1ss0nd <v1ss0nd@proton.me>
2024-01-15 12:01:20 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 from twitchio.ext import commands
 from datetime import datetime
 from pathlib import Path
-from git import Repo
 import asyncio
 import json
 import shutil
@@ -9,33 +8,35 @@ import subprocess
 
 class vsndbot(commands.Bot):
     def __init__(self):
-        super().__init__(token='', prefix='#', initial_channels=[''])
-        for command in [path.stem for path in Path("commands").glob("*py")]:
-            self.load_module(f"commands.{command}")
-            self.user_langs = {}
+        super().__init__(token='', prefix='#', initial_channels=['']) # Token, prefix and channels
+
+        for command in [path.stem for path in Path("commands").glob("*py")]: # Get folder with modules
+            self.load_module(f"commands.{command}") # Get and import modules
+            self.user_langs = {} # Local dictionary for custom language
 
     async def event_ready(self):
         print('Ready')
-        await bot.get_channel('vsndbot').send(f"/me Reconnected ZULULR TeaTime")
+        await bot.get_channel('vsndbot').send(f"/me Reconnected") # Send message into bot chat when ready
 
-    async def event_message(self, message):
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
+    async def event_message(self, message): # Print messages in the terminal
+        now = datetime.now() # Time
+        current_time = now.strftime("%H:%M:%S") 
         try: 
-            print(f"({message.channel.name}) [{current_time}] {message.author.name}: {message.content}")
-        except AttributeError:
-            print(f"({message.channel.name}) [{current_time}] vsndbot: {message.content}")
+            print(f"({message.channel.name}) [{current_time}] {message.author.name}: {message.content}") # Messages from regular chatters
+        except AttributeError: # Since twitchio returns an error when it tries to get bot's nickname, I just hid the error message using "except"
+            print(f"({message.channel.name}) [{current_time}] vsndbot: {message.content}") # Message from the bot
             return
         await self.handle_commands(message)
 
-    async def event_command_error(self, ctx, error: Exception) -> None:
+    async def event_command_error(self, ctx, error: Exception) -> None: # Messages when cooldown is hitten
         if isinstance(error, commands.CommandOnCooldown):
             if error.command.name == "pig":
                 await ctx.reply(f"You have to wait a few minutes to catch more pigs. (Cooldown 25 minutes)")
             if error.command.name == "sell":
                 await ctx.reply(f"You've sold too many pigs. Next sale in 1 hours.")
 
-    @commands.command(aliases = ("language",))
+
+    @commands.command(aliases = ("language",)) # Set preferred language of the bot outputs
     @commands.cooldown(1, 2, commands.Bucket.user)
     async def lang(self, ctx, lang):    
         with open("user_langs.json", "r") as f:
@@ -47,19 +48,19 @@ class vsndbot(commands.Bot):
             json.dump(self.user_langs, f)        
         await ctx.reply(f"Your language has been changed to {lang}")
         
-    @commands.command()
+
+    @commands.command() 
     async def dev(self, ctx: commands.Context, action, *, name: str = None):
-        if ctx.author.name == "v1ss0nd":
-            token = ""
-            if action.startswith("reload"):
+        if ctx.author.name == "v1ss0nd": # Your name
+            if action.startswith("reload"): # Reload module
                 try:
-                    self.reload_module(f'commands.{name}')
+                    self.reload_module(f'commands.{name}') 
                     await ctx.reply(f'Module "{name}" successfully reloaded')
                 except Exception as e:   
                     await ctx.reply(f"{e}")
                 return
                 
-            if action.startswith("load"):
+            if action.startswith("load"): # Load module
                 try:
                     self.load_module(f'commands.{name}')
                     await ctx.reply(f'Module "{name}" successfully loaded')
@@ -67,25 +68,14 @@ class vsndbot(commands.Bot):
                     await ctx.reply(f"{e}")
                 return
 
-            if action.startswith("unload"):
+            if action.startswith("unload"): # Unload module
                 try:
                     self.unload_module(f'commands.{name}')
                     await ctx.reply(f'Module "{name}" successfully unloaded')
                 except Exception as e:   
                     await ctx.reply(f"{e}")
                 return
-            
-            if action.startswith("clone"):
-                try:
-                    repo_url = f"https://{token}:x-oauth-basic@github.com/v1ss0nd/vsndbot"
-                    repo = Repo.clone_from(repo_url, "./temp")
-                    Path(f"./temp/{name}").rename(f"./{name}")
-                    shutil.rmtree("./temp")
-                    await ctx.reply(f'File "{name}" successfully cloned from ./v1ss0nd/vsndbot/{name}')
-                except Exception as e:   
-                    await ctx.reply(f"{e}")
-                return
-            if action.startswith("cmd"):
+            if action.startswith("cmd"): # Execute commands from chat!
                 try:
                     message = subprocess.getoutput(f'{name}')
                     await ctx.reply(f"{message}")
